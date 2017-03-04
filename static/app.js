1e02b8250997345b10e73b107cdb01aaa066aeb2
Daniel Yovchev <daniel3d@ymail.com>
2017-03-04 02:57:32 +0000

    Material Design css
    - Add material design css
    - Remove menu use title for nav
    - Add button for adding markers
    - Add Dialog
    
    TODO: Fix width of the dialogbox.

--- static/app.js
+++ static/app.js
@@ -1,7 +1,7 @@
 var GoogleMapViewModel = function() {
 	var 
 		self = this,
-		TopMenu = document.getElementById('top-menu'),
+		weatherApiKey = '&APPID=5e3c6273faa62f18231cc3cfc0c4e96d';
 		MapCanvas = document.getElementById('map-canvas'),
 		PanelLeft = document.getElementById('panel-left'),
 		LocationSearchInput = document.getElementById('location-search'),
@@ -23,16 +23,33 @@ var GoogleMapViewModel = function() {
 		// Lissen for place changed by the location search
 		LocationSearchBox.addListener('place_changed', function() {
 			var location = LocationSearchBox.getPlace();
-
+			var city = null;
+			var country = null;
+			var neigborhood = null;
+			var lat = location.geometry.location.lat();
+			var lng = location.geometry.location.lng();
 			// This is the 2 fields we will restrict the markers
 			// country and administrative_area_level_1
 			// make sure we restrict the types to (cities)
 			$.each(location.address_components, function( index, value ){
-				if(value.types[0] == 'country' || value.types[0] == 'administrative_area_level_1'){
-					console.log(value);
+				if(	value.types[0] == 'country') {
+					country = value.short_name;
+				}
+				if(	value.types[0] == 'administrative_area_level_1') {
+					neigborhood = value.short_name;
+				}
+				if(	value.types[0] == 'postal_town') {
+					city = value.short_name;
 				}
 			});
 
+			// Get whether...
+			var whetherUrlRequest = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+weatherApiKey;
+			$.getJSON(whetherUrlRequest, function(data){ 
+				console.log(data);
+			});
+
+			// Set the map center.
 			GoogleMap.fitBounds(location.geometry.viewport);
 		});
 
@@ -42,7 +59,6 @@ var GoogleMapViewModel = function() {
 		});
 
 		// Set Ui elements on the map.
-		GoogleMap.controls[google.maps.ControlPosition.TOP_RIGHT].push(TopMenu);
 		GoogleMap.controls[google.maps.ControlPosition.TOP_LEFT].push(PanelLeft);
 
 		// UI Mobile
