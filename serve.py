29ea84950cf2bdc0f182ea4c5654f6f5adeab68d
Daniel Yovchev <daniel3d@ymail.com>
2017-03-10 00:18:49 +0000

    Work on Bonds detection
    - Worrk on sending api bonds cordianates.
    - Few code refactorings
    
    TODO:
    - Create sql search based on the bonds.
    - Load and delete markers based on curent bonds.

--- serve.py
+++ serve.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort, make_response, render_template
+from flask import Flask, jsonify, abort, make_response, render_template, request
 
 app = Flask(__name__)
 
@@ -9,29 +9,35 @@ def hello():
 markers = [
     {
         'id': 1,
-        'image': u'',
-        'title': u'',
-        'description': u''
+        'image': u'https://c1.staticflickr.com/6/5788/21200534656_39e2820ec5_k.jpg',
+        'title': u'London location',
+        'description': u'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     },
     {
         'id': 2,
-        'image': u'',
-        'title': u'',
-        'description': u''
+        'image': u'https://c1.staticflickr.com/1/686/21200536326_4d35ac8a36_k.jpg',
+        'title': u'London location 2',
+        'description': u'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     }
 ]
 
-    
+
 @app.route('/api/markers', methods = ['GET'])
 def get_tasks():
-    return jsonify( { 'markers': markers } )
+    nw = request.args.getlist('nw[]')
+    sw = request.args.getlist('sw[]')
+
+    if nw and sw:
+        return jsonify([nw, sw])
+    else:
+        return jsonify(markers)
 
 @app.route('/api/markers/<int:marker_id>', methods = ['GET'])
 def get_task(marker_id):
     marker = filter(lambda m: m['id'] == marker_id, markers)
     if len(marker) == 0:
         abort(404)
-    return jsonify( { 'marker': marker[0] } )
+    return jsonify(marker[0])
 
     
 @app.errorhandler(400)
--- static/app.js
+++ static/app.js
@@ -1,141 +1,136 @@
-// Extract address components from location.
-// Use this function to return { city, country, neigborhood }
-function extractAddressComponents(location) {
-	var 
-	result = { 
-		city: null,
-		country: null,
-		neigborhood: null
-	},
-	lat = location.geometry.location.lat(),
-	lng = location.geometry.location.lng();
-	// This is the 2 fields we will restrict the markers
-	// country and administrative_area_level_1
-	// make sure we restrict the types to (cities)
-	$.each(location.address_components, function(index, value) {
-		switch(value.types[0]) {
-			case 'country':
-				result.country = value.short_name;
-				break;
-			case 'administrative_area_level_1':
-				result.neigborhood = value.short_name;
-				break;
-			case 'postal_town':
-				result.city = value.short_name;
-				break;
-		}
-	});
-	return result;
-}
-
 // Google map api object
 // use to interact with the google map
-function GoogleMap(mapCenterlatlng) {
+// -----------------------------------------------------------------------------
+function GoogleMap(initAddress) {
 	var
 	self = this,
 	$canvas = $('#map-canvas'),
 	$panelLeft = $('#panel-left'),
 	$searchInput = $('#location-search'),
+	mapBondsChanging = false,
 	geocoder = new google.maps.Geocoder,
 	infoWindow = new google.maps.InfoWindow,
 	searchBox = new google.maps.places.Autocomplete($searchInput[0], {
 		types: ['(cities)']
 	});
 
-	self.map = new google.maps.Map($canvas[0], {
-		mapTypeControl: false,
-		fullscreenControl: false,
-		center: mapCenterlatlng,
-		backgroundColor: 'none',
-		zoom: 11
-	});
-
-	// Set map center based on the UI width
-	google.maps.Map.prototype.setUiCenter = function(latlng) {
+	// Calculate pan for the map based on the UI width
+	var panUI = function() {
 		uiWidth =  $panelLeft.width();
 		winWidth = $(window).width();
-		panBy = (winWidth > 640) ? -Math.abs((winWidth/2) - ((winWidth - uiWidth)/2)) : 0;
-		console.log([uiWidth, winWidth, panBy]);
-		self.map.setCenter(latlng)
-		self.map.panBy(panBy, 0);
+		return (winWidth > 640) ? -Math.abs((winWidth/2) - ((winWidth - uiWidth)/2)) : 0;
 	}
 
-	// Set position of the Ui elements on the map.
-	self.map.controls[google.maps.ControlPosition.TOP_LEFT].push($panelLeft[0]);
-
-	// Move the map to the side
-	// TODO: on mobile we need to make sure this dont execute...
-	self.map.panBy(-200, 0);
-
-	// Lissen for place changed by the location search
-	searchBox.addListener('place_changed', function() {
-		var location = searchBox.getPlace();
-
-		// Set the map center.
-		self.map.fitBounds(location.geometry.viewport);
+	// Create the map object.
+	self.map = new google.maps.Map($canvas[0], { 
+		zoom: 11,
+		mapTypeControl: false, 
+		backgroundColor: 'none',
+		fullscreenControl: false
 	});
 
 	// Load map style async...
-	$.getJSON('/static/map-style.json', function(data){ 
+	$.getJSON('/static/map-style.json', function(data) { 
 		self.map.setOptions({styles:data});
 	});
 
-	// Center the map on resizing.
+	// Set map center based on the initial address.
+	geocoder.geocode({address: initAddress}, function(results, status) {
+		if(status == 'OK') {
+			self.map.fitBoundsWithPan(results[0].geometry.viewport, panUI());
+		} else { // Set defaul center.
+			self.map.setCenterWithPan({lat: 51.507378, lng: -0.128171}, panUI());
+		}
+	});
+
+	// Add the ui elements to the map.
+	self.map.controls[google.maps.ControlPosition.TOP_LEFT].push($panelLeft[0]);
+
+	// Lissen for place changed when selected from the location search.
+	searchBox.addListener('place_changed', function() {
+		var location = searchBox.getPlace();
+		self.map.fitBoundsWithPan(location.geometry.viewport, panUI());
+	});
+
+	// Center the map when window is resized.
 	google.maps.event.addDomListener(window, "resize", function() {
 		var center = self.map.getCenter();
 		google.maps.event.trigger(self.map, "resize");
 		self.map.setCenter(center);
 	});
 
-	// Create marker to the map
-	// ------------------------------------------------------------------------
-	self.createMarker = function(latlng, iconSvgUrl, animation) {
-		var marker = new google.maps.Marker({
-			position: latlng,
-			map: self.map,
-			icon: new google.maps.MarkerImage(iconSvgUrl, 
-				null, null, null, new google.maps.Size(40,40)),
-			animation: animation,
-			draggable: true,
-			optimized: false
+	// Fire callback event when bonds get changed delay with 1000ms
+	self.onBondsChange = function(callback) {
+		google.maps.event.addListener(self.map,'bounds_changed', function(){
+			if(!mapBondsChanging) {
+				setTimeout(function(){
+					mapBondsChanging = false;
+					var bounds = self.map.getBounds();
+					var nw = bounds.getNorthEast()
+					var sw =  bounds.getSouthWest();
+					callback({
+						nw: [nw.lat(), nw.lng()],
+						sw: [sw.lat(), sw.lng()]
+					});
+				}, 1000);
+			}
+			mapBondsChanging = true;
 		});
-		
-		// Center the map to the new marker
-		self.map.setZoom(17);
-		self.map.setUiCenter(marker.position);
 	}
 
-	google.maps.event.addListener(self.map, 'click', function(event) {
-		self.createMarker(event.latLng, "static/maps-and-flags.svg", google.maps.Animation.DROP);
-	});
 }
 
 
 // The app knockoutJs view
-// Here we initilize every thing..
+// -----------------------------------------------------------------------------
 var NeighborhoodMapViewModel = function() {
 	var 
 	self = this,
-	googleMap = new GoogleMap({lat: 51.507378, lng: -0.128171});
+	googleMap = new GoogleMap("London, UK");
 
+	// Set observables
+	self.ListOfMarkers = ko.observableArray([]);
 
-	// Add marker
-	self.addMarker = function() {
-		googleMap.createMarker(googleMap.map.getCenter(), "static/push-pin.svg", google.maps.Animation.BOUNCE);
-	}
+	// Make sure we update the markers when bonds get changed.
+	googleMap.onBondsChange(function(bonds) {
+		$.getJSON('/api/markers?' + $.param(bonds), function(data){ 
+			console.log(data);
+			//self.ListOfMarkers(data);
+		});
+	});
 
-	// UI Mobile
-	//------------------------------------------------------------------------
+	// UI functionality --------------------------------------------------------
 	self.panelVisible = ko.observable(true);
 	self.tooglePanel = function() {
 		self.panelVisible(self.panelVisible() ? false : true);
+	}
+
+	self.addMarker = ko.observable(false);
+	self.toogleAddMarker = function() {
+		if(!self.panelVisible()){
+			self.tooglePanel();
+		}
+		self.addMarker(self.addMarker() ? false : true);
 	}
 
 }
 
 // Execute this function
 // when google maps script is ready
+// -----------------------------------------------------------------------------
 function googleMapsJsLoaded() {
+
+	// Set map center and add pan
+	google.maps.Map.prototype.setCenterWithPan = function(latlng, panBy) {
+		this.setCenter(latlng)
+		this.panBy(panBy, 0);
+	}
+
+	// Fit bounds and add pan
+	google.maps.Map.prototype.fitBoundsWithPan = function(viewport, panBy) {
+		this.fitBounds(viewport);
+		this.panBy(panBy, 0);
+	}
 
 	// Boostrap the application ui.
 	ko.applyBindings(new NeighborhoodMapViewModel());
